4d181c89b07f3ecf142fef73c8c356713eb80e4f
Taninchot P <taninchot.art@gmail.com>
2022-07-07 11:38:34 +0700

    add log when connect to database

--- src/mongoose.js
+++ src/mongoose.js
@@ -2,9 +2,14 @@ const mongoose = require('mongoose');
 const logger = require('./logger');
 
 module.exports = function (app) {
+  console.log("Connecting to Database ...");
   mongoose.connect(
     app.get('mongodb')
-  ).catch(err => {
+  ).then( () => {
+    console.log("Connected to Database Successfully");
+    }
+  )
+  .catch(err => {
     logger.error(err);
     process.exit(1);
   });
