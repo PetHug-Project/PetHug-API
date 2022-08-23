0369033f710349a0df7bff251fdfa610fc54a66b
Taninchot P <taninchot.art@gmail.com>
2022-08-23 14:12:37 +0700

    [UPDATE] collect user_id instead firebase_uid

--- src/services/board_comment_reply/board_comment_reply.class.js
+++ src/services/board_comment_reply/board_comment_reply.class.js
@@ -7,8 +7,9 @@ exports.BoardCommentReply = class BoardCommentReply extends Service {
   }
 
   async createBoardCommentReply(data, params) {
-    let { user_id } = params.decodeAccessToken
-    data.user_id = user_id
+    let { uid } = params.decodeAccessToken
+    let user = await this.app.service("users-service").getDataFromFirebaseUid(uid)
+    data.user_id = user._id
     let result = await super.create(data, params)
     this.app.service("board-service").addComment(data.board_id, result._id.toString())
     return result
