72e58b0626a0ea19b13e22b19bd05343d2ee2f16
Taninchot P <taninchot.art@gmail.com>
2022-09-27 13:59:15 +0700

    [UPDATE] match with case insensitive

--- src/services/pets/pets.class.js
+++ src/services/pets/pets.class.js
@@ -115,9 +115,9 @@ exports.Pets = class Pets extends Service {
             {
               $match: {
                 $or: [
-                  { 'pet_breed': { $regex: search } },
-                  { 'pet_lost_details.petLostDetail': { $regex: search } },
-                  { 'pet_lost_details.petLostLocation': { $regex: search } }
+                  { 'pet_breed': { $regex: search, $options: "i" } },
+                  { 'pet_lost_details.petLostDetail': { $regex: search, $options: "i" } },
+                  { 'pet_lost_details.petLostLocation': { $regex: search, $options: "i" } }
                 ]
               }
             },
@@ -136,6 +136,15 @@ exports.Pets = class Pets extends Service {
           ],
           pageInfo: [
             { $match: { isLost: true } },
+            {
+              $match: {
+                $or: [
+                  { 'pet_breed': { $regex: search, $options: "i" } },
+                  { 'pet_lost_details.petLostDetail': { $regex: search, $options: "i" } },
+                  { 'pet_lost_details.petLostLocation': { $regex: search, $options: "i" } }
+                ]
+              }
+            },
             { $group: { _id: null, count: { $sum: 1 } } },
           ]
         }
